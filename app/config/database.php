b207111b8159fcff231ef3b119e3d59b1c2e5c5a
Ryan Tablada <ryan.tablada@gmail.com>
2013-06-05 12:54:34 -0400

    Cleaned up database config. Closed #3 with last commit

--- app/config/database.php
+++ app/config/database.php
@@ -46,12 +46,6 @@ return array(
 
 	'connections' => array(
 
-		'sqlite' => array(
-			'driver'   => 'sqlite',
-			'database' => __DIR__.'/../database/production.sqlite',
-			'prefix'   => '',
-		),
-
         'mysql' => array(
             'driver'    => 'mysql',
             'host'     => isset( $_SERVER["DB1_HOST"] ) ? $_SERVER["DB1_HOST"] : '',
@@ -63,26 +57,6 @@ return array(
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
         ),
-
-		'pgsql' => array(
-			'driver'   => 'pgsql',
-			'host'     => 'localhost',
-			'database' => 'database',
-			'username' => 'root',
-			'password' => '',
-			'charset'  => 'utf8',
-			'prefix'   => '',
-			'schema'   => 'public',
-		),
-
-		'sqlsrv' => array(
-			'driver'   => 'sqlsrv',
-			'host'     => 'localhost',
-			'database' => 'database',
-			'username' => 'root',
-			'password' => '',
-			'prefix'   => '',
-		),
 
 	),
 
